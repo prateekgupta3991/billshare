29286f93bf12dfc448b0e92b59e158a1be5f939c
Prateek Gupta <prateekgupta.3991@gmail.com>
2018-08-25 23:21:05 +0530

    refactored controllers

--- src/main/java/com/share/bill/controllers/GroupController.java
+++ src/main/java/com/share/bill/controllers/GroupController.java
@@ -2,60 +2,34 @@ package com.share.bill.controllers;
 
 import com.share.bill.dto.GroupRequestDto;
 import com.share.bill.dto.GroupResponseDto;
-import com.share.bill.dto.UserRequestDto;
-import com.share.bill.dto.UserResponseDto;
 import com.share.bill.entities.Group;
-import com.share.bill.entities.User;
 import com.share.bill.services.BillShareService;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
 @RequestMapping(value="/v1")
-public class BillSharePrimaryController extends AbstractController {
+public class GroupController extends AbstractController {
 
 	@Autowired
     private BillShareService billShareServiceImpl;
-
-	public void setBillShareServiceImpl(BillShareService billShareServiceImpl) {
-		this.billShareServiceImpl = billShareServiceImpl;
-	}
 
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
 		ModelAndView model = new ModelAndView("HelloWorldPage");
 		model.addObject("msg", "hello world");
 		return model;
-	}
-
-	@RequestMapping(value="/user", method=RequestMethod.POST)
-	public ResponseEntity<User> createNewUser(@RequestBody UserRequestDto json ) {
-
-		User usr = billShareServiceImpl.addNewUser(json);
-		return new ResponseEntity<>(usr, HttpStatus.OK);
-	}
-
-	@RequestMapping(value="/user", method=RequestMethod.GET)
-	public ResponseEntity<List<UserResponseDto>> getUser() {
-
-		List<UserResponseDto> userList = billShareServiceImpl.getAllUsers();
-		return new ResponseEntity<>(userList, HttpStatus.OK);
-	}
-
-	@RequestMapping(value="/user/{userid}", method=RequestMethod.GET)
-	public ResponseEntity<UserResponseDto> getUserById(@PathVariable(value = "userid") Long userId) {
-
-        UserResponseDto user = billShareServiceImpl.getUser(userId);
-		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 
 	@RequestMapping(value="/group", method=RequestMethod.POST)
